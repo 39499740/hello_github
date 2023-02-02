f882ee98c399ffc571461787fcf8583102c0409c
haoyi <39499740@qq.com>
2023-02-02 16:22:07 +0800

    ✨ feat: 添加Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,31 @@
+/*
+ * @Author: haoyi 39499740@qq.com
+ * @Date: 2023-02-01 22:07:35
+ * @LastEditors: haoyi 39499740@qq.com
+ * @LastEditTime: 2023-02-02 16:21:08
+ * @FilePath: /HelloGithub/lib/main.dart
+ * @Description: 
+ * Bilibili 天国的502
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+ */
+import 'dart:io';
+
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hello_github/providers/global_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (_) => GlobalProvider()),
+  ], child: MyApp()));
+  if (Platform.isAndroid) {
+    // android 状态栏透明
+    SystemUiOverlayStyle systemUiOverlayStyle =
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/providers/global_provider.dart
@@ -0,0 +1,14 @@
+/*
+ * @Author: haoyi 39499740@qq.com
+ * @Date: 2023-02-02 16:10:28
+ * @LastEditors: haoyi 39499740@qq.com
+ * @LastEditTime: 2023-02-02 16:18:48
+ * @FilePath: /HelloGithub/lib/providers/global_provider.dart
+ * @Description: 全局状态管理
+ * Bilibili 天国的502
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+ */
+
+import 'package:flutter/material.dart';
+
+class GlobalProvider with ChangeNotifier {}
